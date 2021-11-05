87d128ba257ed40d5f132acb65f5cead0a83fcdf
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-11-05 10:08:29 +0100

    02 He podido realizar las espectativas a realizar

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -23,7 +23,15 @@ public class MaquinaExpendedoraSimple {
         estacionOrigen = "León";
         estacionDestino = "Asturias";
     }
-
+    
+    public MaquinaExpendedoraSimple(int precioDelBillete, String destino) {
+        precioBillete = precioDelBillete;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "León";
+        estacionDestino = destino;
+    }
+    
     /**
      * Devuelve el precio del billete
      */
